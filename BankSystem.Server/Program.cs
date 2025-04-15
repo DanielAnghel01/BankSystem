743e0a6c8db1393ede2124865334ccfd44eac176
Daniel <angheldaniel674@gmail.com>
2025-04-15 23:43:37 +0300

    .

--- BankSystem.Server/Program.cs
+++ BankSystem.Server/Program.cs
@@ -11,7 +11,10 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngularDev", policy =>
     {
-        policy.WithOrigins("https://bank-system-web.azurewebsites.net") // Your Angular dev server
+        policy.WithOrigins(
+            "https://bank-system-web.azurewebsites.net",
+            "https://localhost:57322"
+            ) // Your Angular dev server
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials(); // optional if you're using cookies
