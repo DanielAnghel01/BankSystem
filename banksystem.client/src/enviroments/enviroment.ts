8a29abb833c3d40f56d11afa400ff5bc9cade715
Daniel <angheldaniel674@gmail.com>
2025-05-15 02:26:24 +0300

    bug fix

--- banksystem.client/src/enviroments/enviroment.ts
+++ banksystem.client/src/enviroments/enviroment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  //apiUrl: 'https://bank-system-web.azurewebsites.net/api/auth/login',
-  apiUrl: 'https://localhost:7022/api/auth/login',
+  apiUrl: 'https://bank-system-web.azurewebsites.net/api/auth/login',
+  //apiUrl: 'https://localhost:7022/api/auth/login',
   apiUrlRegister: 'https://localhost:7022/api/auth/register'
 }
