40a58665a9fb3867ea4d3110a65f1d1c423453cb
Daniel <angheldaniel674@gmail.com>
2025-05-29 14:06:22 +0300

    polished frontend

--- banksystem.client/src/app/pages/privacy/privacy.component.ts
+++ banksystem.client/src/app/pages/privacy/privacy.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   standalone: true,
   selector: 'app-privacy',
   templateUrl: './privacy.component.html',
-  styleUrl: './privacy.component.css'
+  styleUrl: './privacy.component.css',
+  imports: [
+    MatCardModule,
+    MatDividerModule,
+    MatListModule,
+  ]
 })
 export class PrivacyComponent {
 
